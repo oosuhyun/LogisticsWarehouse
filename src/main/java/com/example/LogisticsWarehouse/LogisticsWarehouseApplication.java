ff17bc0b8dbbfdfb5d3ee21c987958b4e17a9362
oosuhyun <tngusimg@naver.com>
2023-08-07 17:58:07 +0900

    Initial commit

---
+++ src/main/java/com/example/LogisticsWarehouse/LogisticsWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogisticsWarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsWarehouseApplication.class, args);
+	}
+
+}
